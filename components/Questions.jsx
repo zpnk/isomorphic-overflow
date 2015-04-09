75c8dd620f1949e36634218ed8d60cd28e5d2b04
Zach K <z@zpnk.me>
2015-04-09 13:34:20 -0700

    fetch component data client-side

--- components/Questions.jsx
+++ components/Questions.jsx
@@ -1,9 +1,46 @@
-var React = require('react')
+var React      = require('react'),
+    superagent = require('superagent')
+
+var API_URL = 'https://api.stackexchange.com/2.2/questions?pagesize=10&sort=hot&tagged=javascript&site=stackoverflow'
 
 var Questions = React.createClass({
 
+  statics: {
+    fetch: function(params, callback) {
+      superagent.get(API_URL, function(err, res) {
+        callback({questions: res.body.items})
+      })
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      questions: []
+    }
+  },
+
+  componentDidMount: function() {
+    if (this.state.questions.length) return
+
+    var self = this
+    Questions.fetch({}, function(data) {
+      self.setState({questions: data.questions})
+    })
+  },
+
   render: function() {
-    return <p>The 10 hottest javascript questions on Stack Overflow</p>
+    var questions = this.state.questions.map(function(q) {
+      return <li key={q.question_id}>{q.title}</li>
+    })
+
+    return (
+      <div>
+        <p>The 10 hottest javascript questions on Stack Overflow:</p>
+        <ul>
+          {questions}
+        </ul>
+      </div>
+    )
   }
 
 })
